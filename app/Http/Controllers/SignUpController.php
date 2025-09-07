e2afe78e24d24718ad850d457f304a5ae3f4a424
ViniFigueiredo <viniciusfigueiredouchoa@gmail.com>
2025-09-07 11:07:17 -0300

    Cadastro de Usuário feito

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -5,15 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator; // <- IMPORTANTE ADICIONAR ESSA LINHA EU ESTOU LOUCO
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SignUpController extends Controller
 {
     public function store(Request $request)
     {
-        $campos = $request->all();
-
-        Log::info($campos);
-        
-        return redirect("/")->with("status", "Signed In!");
+         $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'cpf' => 'required|string|max:14|unique:users',
+            'phone' => 'required|string|max:15',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+ $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'cpf' => $request->cpf,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+            'role' => 'cliente',
+        ]);
+        // Log::info('User registered: ', $request->all());
+        return redirect("/")->with('status', 'Cadastro realizado com sucesso! Por favor, faça login.');
     }
 }
